f86320fd3ae11760074aa2011f308c2820139fbd
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-11-05 20:03:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-System.register([], function($__export, $__moduleContext) {
-    var x;
-    function p() {
-      x = 10;
-    }
-    $__export({
-      x: undefined,
-      p: p
-    });
-    return {
-      // setters: [], // (setters can be optional when empty)
-      execute: function() {
-        p();
-        console.log(x)
-      }
-    };
-  });
+define([],function(){
+  //TODO: Implements the AMD module
+  console.log("Module 3: loaded");
+});
