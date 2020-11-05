c41fccc3007d766be4dda4ed4c96128db7879c02
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-11-05 19:44:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-export default {
-  init() {
-    console.warn('Widget inited');
-  }
-}
+System.register([], function($__export, $__moduleContext) {
+    var x;
+    function p() {
+      x = 10;
+    }
+    $__export({
+      x: undefined,
+      p: p
+    });
+    return {
+      // setters: [], // (setters can be optional when empty)
+      execute: function() {
+        p();
+        console.log(x)
+      }
+    };
+  });
