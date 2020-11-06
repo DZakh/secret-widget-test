3d63a64b43767a3afefb9edbb9a868fa03965c85
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-11-06 19:24:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,12 @@ define([], function () {
   return {
     initLead(token) {
       // VERSION 1
-      // console.warn('WIDGET initLead with token: ', token);
+      console.warn('WIDGET initLead with token: ', token);
 
-      // return () => {
-      //   console.warn('WIDGET initLead destroyed');
-      // };
+      return () => {
+        console.warn('WIDGET initLead destroyed');
+      };
       // VERSION 2
-      return new Promise((resolve) => {
-        console.warn('WIDGET initLead with token: ', token);
-
-        setTimeout(() => {
-          resolve(() => {
-            console.warn('WIDGET initLead destroyed');
-          });
-        }, 3000);
-      });
-      // VERSION 3
       // console.warn('WIDGET initLead with token: ', token);
     },
     registerWidgetsBarSlot(el) {
